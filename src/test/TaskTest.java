c7e8bf2c0885e3f77a2c24ff720d9e422867ec90
IVPetrov682 <161107540+IVPetrov682@users.noreply.github.com>
2024-07-15 21:02:29 +0800

    Add files via upload

---
+++ src/test/TaskTest.java
@@ -0,0 +1,49 @@
+package test;
+
+
+import logic.Counter;
+import logic.ManagerStatus;
+import logic.Status;
+import logic.TypeTask;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class TaskTest {
+
+    static HashMap<Integer, ManagerStatus> numberTask; // создали таблицу номер/задача
+    static Counter number = new Counter(); // Создали объект класса счетчика
+
+
+    String testTask1 = "Тестовая задача 1";
+    String testTask2 = "Тестовая задача 2";
+    private Duration duration;
+    private LocalDateTime startTime;
+
+    public TaskTest() {
+    }
+
+    @Test
+    void addNTestTask() {
+
+        numberTask = new HashMap<>();
+        number.increment(); //
+        numberTask.put(number.getCount(), new ManagerStatus(TypeTask.TASK, "Изучить Java", Status.INPROGRESS, testTask1, null, null, null));// сохранили в таблицу
+
+        number.increment(); //
+        numberTask.put(number.getCount(), new ManagerStatus(TypeTask.TASK, "Изучить Java", Status.INPROGRESS, testTask2, null, null, null)); // сохранили в таблицу
+
+        for (Integer keyPrint : numberTask.keySet()) {
+            String taskPrint = numberTask.get(keyPrint).getDescription();
+            System.out.println("Индификатор № " + keyPrint + ". " + taskPrint + ".");
+            int notEror = 0; // переменная для проверки счетчика
+            assertNotNull(keyPrint, "Счетчик работает не корректно");
+            assertNotEquals(keyPrint, notEror, "Счетчик работает не корректно");
+        }
+    }
+}
