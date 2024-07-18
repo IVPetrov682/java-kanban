c6fa633c5cf549285fc8f8823fa25a2e039b3724
IVPetrov682 <161107540+IVPetrov682@users.noreply.github.com>
2024-07-18 21:20:02 +0800

    Task 8 last
    Task 8 last

---
+++ test/ManagersTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import logic.HistoryManager;
+import logic.Managers;
+import logic.TaskManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+class ManagersTest {
+
+    @Test
+    void newTaskManager() {
+        TaskManager taskManager = Managers.getDefault();
+
+        assertNotNull(taskManager, "Менеджер не проинициализирован");
+    }
+
+    @Test
+    void newHistoryManager() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+
+        assertNotNull(historyManager, "Менеджер не проинициализирован");
+    }
+
+}
