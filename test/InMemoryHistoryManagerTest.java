5ba291c649a9be04980d0fc6dd1188bf187f8da5
IVPetrov682 <161107540+IVPetrov682@users.noreply.github.com>
2024-07-23 20:52:55 +0800

    Task 8 update
    Task 8 update

---
+++ test/InMemoryHistoryManagerTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import logic.*;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class InMemoryHistoryManagerTest {
+
+    private Duration duration;
+    private LocalDateTime startTime;
+
+    @Test
+    void checkSizeOfRequestHistory() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+        AllTask allTask = new AllTask(1, TypeTask.TASK, "Вася", Status.NEW, "Описание", duration, startTime, null);
+        final int sizeFromRequestHistoryShouldBe = 1;
+        final int sizeForCheckRequestSize = 9;
+
+        for (int i = 0; i <= sizeForCheckRequestSize; i++) {
+            historyManager.add(allTask);
+        }
+        List<AllTask> exampleOfRequestHistoryList = historyManager.getHistory();
+
+        assertEquals(sizeFromRequestHistoryShouldBe, exampleOfRequestHistoryList.size(), "Ограничение сохранения повтора "
+                + "не работает");
+    }
+
+    @Test
+    void add() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+        AllTask allTask = new AllTask(1, TypeTask.TASK, "Вася", Status.NEW, "Описание", duration, startTime, null);
+        historyManager.add(allTask);
+        final List<AllTask> history = historyManager.getHistory();
+        assertNotNull(history, "Список заполнен");
+        assertEquals(1, history.size(), "Список заполнен");
+    }
+
+}
